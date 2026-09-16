0d8b6f0e0ef8e126206834a2009428fa8c9ffa46
g5v4jvv5hs-web <g5v4jvv5hs@privaterelay.appleid.com>
2026-09-16 20:46:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2623,8 +2623,8 @@ CREATE TABLE IF NOT EXISTS support_messages (
          tap_count DESC,
          last_tap_at ASC
        )`,
-      `ALTER TABLE users
-       ADD COLUMN IF NOT EXISTS display_name TEXT`,
+      `      `ALTER TABLE users
+       ADD COLUMN IF NOT EXISTS photo_url TEXT`,
 
       `ALTER TABLE users
        ADD COLUMN IF NOT EXISTS ton_wallet_address TEXT`,
